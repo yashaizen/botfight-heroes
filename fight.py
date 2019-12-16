96c7076d34f53278680f4d7280d2044afb2777c2
Yash <yashaizen@gmail.com>
2019-12-16 13:03:39 +0530

    A fight test from user inputs
    A fight scenario based on user inputs, level 1 test

---
+++ fight.py
@@ -0,0 +1,20 @@
+import main
+def main(a,b):  # Attack function where bot 'a' will attack bot 'b'
+    n = int(input("Enter number of times to attack : "))
+    for i in range(n):
+        a.attack_bot(b)
+        print ('bot a has health ' + str(a.health) + ' and bot b has health ' + str(b.health) + ' and energy ' + str(b.energy) + ' has attack ' + str(b.attack))
+    print ('Enter F to fight again or press enter to quit')
+
+    l = input()
+    if l.lower() == 'f':
+        main(a,b)
+
+
+if __name__ == '__main__':
+    print ('choose 2 bots from `simplebot` , `masterbot`, `primebot`, `legendbot`, `godbot`, `omnibot`')
+    p, q = input('Enter bots a and b such that a will attack b (ex: primebot legendbot): ').split()
+    a = Bot(p)
+    b = Bot(q)
+
+    print (main(a,b))
