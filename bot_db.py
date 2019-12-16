1c4754d14278b413f6bbd5521a2b9f20f7e1c840
Yash <yashaizen@gmail.com>
2019-12-16 12:56:07 +0530

    bot_db.py
    A Database For All Kinds of Bots In The Game

---
+++ bot_db.py
@@ -0,0 +1,61 @@
+bot_db = {                                                      # Bot Database and related details
+    'omnibot' : {
+        'attack': 160,
+        'defense': 140,
+        'energy': 450,
+        'health': 400,
+        'xp': 430,
+        'level': 6,
+        'stack' : 0
+    },
+    'godbot' : {
+        'attack': 140,
+        'defense': 120,
+        'energy': 320,
+        'health': 320,
+        'xp': 280,
+        'level': 5,
+        'stack' : 0
+
+    },
+    'legendbot' : {
+        'attack': 120,
+        'defense': 100,
+        'energy': 240,
+        'health': 270,
+        'xp': 180,
+        'level': 4,
+        'stack' : 0
+
+    },
+    'primebot' : {
+        'attack': 100,
+        'defense': 80,
+        'energy': 180,
+        'health': 170,
+        'xp': 100,
+        'level': 3,
+        'stack' : 0
+
+    },
+    'masterbot' : {
+        'attack': 80,
+        'defense': 60,
+        'energy': 140,
+        'health': 110,
+        'xp': 40,
+        'level': 2,
+        'stack' : 0
+
+    },
+    'simplebot' : {
+        'attack': 60,
+        'defense': 40,
+        'energy': 100,
+        'health': 70,
+        'xp': 0,
+        'level': 1,
+        'stack' : 0
+
+    }
+}
